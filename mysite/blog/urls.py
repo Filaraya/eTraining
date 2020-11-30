824328a9e35250a1d367ebefe41468e75a404b05
Filmon Araya <40590856+Filaraya@users.noreply.github.com>
2020-11-29 22:59:27 -0800

    style and login added

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     # Home page
     path ('', views.index, name ='index'),
     path ('modules/', views.ModuleListView.as_view(), name = 'modules'),# module display url link
-    path ('module/<int:id>', views.ModuleDetailView.as_view(), name= 'module-detail'), # detail information of the list module
+    path ('module/<int:pk>', views.ModuleDetailView.as_view(), name= 'module-detail'), # detail information of the list module
     path ('instructors/', views.InstructorListView.as_view(), name='instructors'),
     path ('instructor/<int:pk>', views.InstructorDetailView.as_view(), name= 'instructor-detail'),
 ]
